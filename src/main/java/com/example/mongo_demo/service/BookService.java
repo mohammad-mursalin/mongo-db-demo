30bbb36299db0e9d5102ad786bbed37c26c9f9e7
mohammad-mursalin <mursalinlamon@gmail.com>
2025-05-03 12:17:46 +0600

    service layer

---
+++ src/main/java/com/example/mongo_demo/service/BookService.java
@@ -0,0 +1,23 @@
+package com.example.mongo_demo.service;
+
+import com.example.mongo_demo.model.Book;
+import com.example.mongo_demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public void addBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+}
