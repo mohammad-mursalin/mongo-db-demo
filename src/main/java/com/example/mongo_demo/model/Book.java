421cc28901b0b4106e79b7701e30e58b8d4f2e58
mohammad-mursalin <mursalinlamon@gmail.com>
2025-05-03 12:17:06 +0600

    model class

---
+++ src/main/java/com/example/mongo_demo/model/Book.java
@@ -0,0 +1,19 @@
+package com.example.mongo_demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    private String id;
+    private String name;
+    private String author;
+
+
+}
