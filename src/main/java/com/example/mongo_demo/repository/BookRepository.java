06b106a549f23b347ce5e37d331af3569dfda1f9
mohammad-mursalin <mursalinlamon@gmail.com>
2025-05-03 12:17:32 +0600

    repository

---
+++ src/main/java/com/example/mongo_demo/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.example.mongo_demo.repository;
+
+import com.example.mongo_demo.model.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends MongoRepository<Book, String> {
+}
